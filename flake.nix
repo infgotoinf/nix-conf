3ad627a4d35a8dd0f2f25d22c3310d49d07e3ce7
Phantasmosis <NanoBitExistence@protonmail.com>
2026-06-10 18:51:10 +0200

    MY FIRST COMMIT LET'S GOOO

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS base flake.nix";
+
+  inputs = {
+  nixpkgs.url = "nixpkgs/nixos-26.05";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake/beta";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  home-manager = {
+     url = "github:nix-community/home-manager/release-26.05";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+ };
+
+outputs = { self, nixpkgs, home-manager, zen-browser, ...} @ inputs: {
+    nixosConfigurations.HoMM3Box = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+    ./configuration.nix
+    home-manager.nixosModules.home-manager
+    {
+        home-manager = {
+         useGlobalPkgs = true;
+         useUserPackages = true;
+         users.phantasm = import ./home.nix;
+         backupFileExtension = "backup";
+        };
+        }
+    ];
+    };
+};
+}
---
+++ home.nix
@@ -0,0 +1,96 @@
+ { config, pkgs, system, inputs, ... }:
+let
+   dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    qtile = "qtile";
+    nvim = "nvim";
+    alacritty = "alacritty";
+    rofi = "rofi";
+    };
+in
+{
+    home.username = "phantasm";
+    home.homeDirectory = "/home/phantasm";
+  home.stateVersion = "26.05";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+        sanitytest = "Welcome";
+};
+ };
+
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name  = "";
+        email = "";
+      };
+      init.defaultBranch = "main";
+    };
+  };
+
+services.flameshot = {
+  enable = true;
+  settings = {
+    General = {
+      savePath = "/home/phantasm/Screenshots";
+      disabledTrayIcon = true;
+      showStartupLaunchMessage = false;
+      saveAsFileExtension = ".png";
+      showDesktopNotification = true;
+      showAbortNotification = false;
+      showHelp = true;
+      showSidePanelButton = true;
+
+
+      # Color Customization
+      uiColor = "#740096";
+      contrastUiColor = "#270032";
+      drawColor = "#ff0000";
+
+      useGrimAdapter = true;
+      disabledGrimWarning = true;
+    };
+  };
+};
+
+
+# xdg.configFile."qtile" = {
+# source = create_symlink "${dotfiles}/qtile/";
+#  recursive = true;
+#};
+#
+#xdg.configFile."nvim" = {
+# source = create_symlink "${dotfiles}/nvim/";
+# recursive = true;
+#};
+#xdg.configFile."alacritty" = {
+# source = create_symlink "${dotfiles}/alacritty/";
+# recursive = true;
+#};
+home.packages = with pkgs; [
+   neovim
+   ripgrep
+   nil
+   nixpkgs-fmt
+   nodejs
+   gcc
+   rofi
+   vesktop
+   steam
+   flameshot
+   grim
+   clementine
+   tk
+   fastfetch
+    zip
+    xz
+    unzip
+    p7zip
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+ ];
+   
+}
